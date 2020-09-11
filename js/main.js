bb36dbddbd850d1785402b920955044efa09936e
Nary <635683+Nary@users.noreply.github.com>
2020-09-11 17:43:33 +0200

    competence + travaux

--- js/main.js
+++ js/main.js
@@ -10,7 +10,9 @@ function show_hide(s1,s2) {
     var toHide = document.getElementById(s1)
     var toShow = document.getElementById(s2)
     toHide.classList.toggle("info_hide");
+    toHide.classList.toggle("windowBody");
     toShow.classList.toggle("info_hide");
+    toShow.classList.toggle("windowBody");
 }
 
 $(function() {
